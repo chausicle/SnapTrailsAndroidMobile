a9c6da9e63adbb4f671216fffc87f9d5c8e74d6c
Justin Chau <chausicle@gmail.com>
2018-06-10 13:19:17 -0700

    Make Post and User class

---
+++ app/src/main/java/com/justinchau/snaptrailsandroidmobile/Post.java
@@ -0,0 +1,64 @@
+package com.justinchau.snaptrailsandroidmobile;
+
+public class Post {
+    private String location;
+    private String description;
+    private String image_url;
+    private String created_at;
+    private User user;
+
+    public Post(
+            String location,
+            String description,
+            String image_url,
+            String created_at,
+            User user) {
+        this.location = location;
+        this.description = description;
+        this.image_url = image_url;
+        this.created_at = created_at;
+        this.user = user;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return image_url;
+    }
+
+    public void setImageUrl(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public String getCreatedAt() {
+        return created_at;
+    }
+
+    public void setCreatedAt(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
+
+
---
+++ app/src/main/java/com/justinchau/snaptrailsandroidmobile/User.java
@@ -0,0 +1,29 @@
+package com.justinchau.snaptrailsandroidmobile;
+
+public class User {
+
+    private String username;
+    private String user_image;
+
+    public User(String username, String user_image) {
+        this.username = username;
+        this.user_image = user_image;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUserImage() {
+        return user_image;
+    }
+
+    public void setUserImage(String user_image) {
+        this.user_image = user_image;
+    }
+}
+
